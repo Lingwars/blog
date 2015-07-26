237a3dad039c469fd276388af18db30839418693
Javier G. Sogo <jgsogo@gmail.com>
2015-07-26 12:07:17 +0200

    url-site

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://lingwars.es'
+SITEURL = 'http://lingwars.github.io/blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
