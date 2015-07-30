58fe275f062d9edaec03366cffaa88bb3b65d059
Javier G. Sogo <jgsogo@gmail.com>
2015-07-30 17:15:07 +0200

    typo

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ TIMEZONE = 'Europe/Madrid'
 DEFAULT_LANG = u'es'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
